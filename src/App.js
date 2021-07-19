9c9aba3abe1a6f2cea6c789589ae1ca524a2f94b
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-07-19 17:53:41 -0600

    titles added for inputs and button

--- src/App.js
+++ src/App.js
@@ -4,43 +4,46 @@ import { GiWeightLiftingUp } from "react-icons/gi"
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <div class="bg-red-300 p-6 rounded-lg shadow-lg">
+      <div className="bg-red-300 p-6 rounded-lg shadow-lg">
       <h1 className="text-5xl bg-black text-white">
         <GiWeightLiftingUp className="inline-block text-white align-top"/>
         Your Workout Schedule
         </h1>
-        <div class="mt-5">
-        <div class="w-1/2 flex mx-auto bg-white rounded-md">
-        <div class="p-5 flex flex-col min-w-full">
-                <label for="workout" class="font-medium text-black"
+        <div className="mt-5">
+        <div className="w-1/2 flex mx-auto bg-white rounded-md">
+        <div className="p-5 flex flex-col min-w-full">
+                <label htmlFor="workout" className="font-medium text-black"
                   >Workout</label
                 >
                 <input
                   type="text"
                   id="workout"
-                  class="w-full py-2 border mt-3 border-gray-300 rounded-md"
+                className="w-full py-2 border mt-3 border-gray-300 rounded-md"
+                title="Add Workout"
           />
-          <div class="mb-5 flex flex-col min-w-full">
-                <label for="date" class="font-medium text-black"
+          <div className="mb-5 flex flex-col min-w-full">
+                <label htmlFor="date" className="font-medium text-black"
                   >Date</label
                 >
                 <input
                   type="text"
                   id="date"
-                  class="w-full py-2 border mt-3 border-gray-300 rounded-md"
+                  className="w-full py-2 border mt-3 border-gray-300 rounded-md"
+                  title="Add Date"
                 />
           </div>
-          <div class="mb-5 flex flex-col min-w-full">
-                <label for="time" class="font-medium text-black"
+          <div className="mb-5 flex flex-col min-w-full">
+                <label htmlFor="time" className="font-medium text-black"
                   >Time</label
                 >
                 <input
                   type="text"
-                  id="timee"
-                  class="w-full py-2 border mt-3 border-gray-300 rounded-md"
+                  id="time"
+                  className="w-full py-2 border mt-3 border-gray-300 rounded-md"
+                  title="Add Time"
                 />
               </div>
-          <button class="align-right text-2xl bg-red-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          <button className="align-right text-2xl bg-red-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" title="Schedule Workout">
             Add Workout
           </button>
             </div>
